dfa6da3f6311893f1b0052f4e6e7da940fcd63da
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2022-11-09 21:18:31 +0530

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { AllRoutes } from "./Components/Routes/AllRoutes";
+import Price from "./Components/Pricing/Price";
 function App() {
   return (
     <Box>
       <Navbar />
       <AllRoutes />
+      
     </Box>
   );
 }
--- src/Components/Routes/AllRoutes.tsx
+++ src/Components/Routes/AllRoutes.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../Pages/Home";
 import { Login } from "../Pages/Login/Login";
-import { Signup } from "../Pages/Signup";
+import { Signup } from "../Pages/Signup"
 
+import Price from "../Pricing/Price";
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+     <Route path="/price" element={<Price/>}/>
     </Routes>
   );
 };
