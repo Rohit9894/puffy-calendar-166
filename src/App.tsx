de6b48bb1938109269c96045bc3d41db030fb966
Rohit Gupta <princerohit9893@gmail.com>
2022-11-09 22:54:55 +0530

    resolved merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { AuthContextProvider } from "./Components/Context/AuthContext";
 import { Footer } from "./Components/Footer/Footer";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { AllRoutes } from "./Components/Routes/AllRoutes";
-import Price from "./Components/Pricing/Price";
 function App() {
   return (
     <AuthContextProvider>
