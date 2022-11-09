ba493c1043e6b96d73a5572a66122ec1f028eedf
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2022-11-09 21:36:22 +0530

    random commit

--- src/Components/Context/AuthContext.jsx
+++ src/Components/Context/AuthContext.jsx
@@ -20,6 +20,7 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currenUser) => {
       setUser(currenUser);
+      console.log("user", user);
     });
     return () => {
       unsubscribe();
